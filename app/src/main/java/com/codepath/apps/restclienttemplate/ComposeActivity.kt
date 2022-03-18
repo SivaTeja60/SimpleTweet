be5ee615de41fedbf51bc28f17b273556620e964
SivaTeja <66307185+SivaTeja60@users.noreply.github.com>
2022-03-18 15:01:18 -0700

    Character Count added

--- app/src/main/java/com/codepath/apps/restclienttemplate/ComposeActivity.kt
+++ app/src/main/java/com/codepath/apps/restclienttemplate/ComposeActivity.kt
@@ -1,11 +1,15 @@
 package com.codepath.apps.restclienttemplate
 
 import android.content.Intent
+import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
 import android.util.Log
 import android.widget.Button
 import android.widget.EditText
+import android.widget.TextView
 import android.widget.Toast
 import com.codepath.apps.restclienttemplate.models.Tweet
 import com.codepath.asynchttpclient.callback.JsonHttpResponseHandler
@@ -15,7 +19,7 @@ class ComposeActivity : AppCompatActivity() {
 
     lateinit var etCompose: EditText
     lateinit var btnTweet: Button
-
+    lateinit var charCount: TextView
     lateinit var client: TwitterClient
 
 
@@ -25,8 +29,37 @@ class ComposeActivity : AppCompatActivity() {
 
         etCompose = findViewById(R.id.etTweetCompose)
         btnTweet = findViewById(R.id.btnTweet)
-
+        charCount = findViewById(R.id.charCount)
         client = TwitterApplication.getRestClient(this)
+
+        // set event for edit text change
+        etCompose.addTextChangedListener(object : TextWatcher {
+            override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+                Log.i(TAG, "Before text change")
+            }
+
+            override fun onTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+                Log.i(TAG, "Text has changed!")
+            }
+
+            override fun afterTextChanged(p0: Editable?) {
+                val lengthOfTweet = etCompose.text.toString().length
+                val remainingChars = 280 - lengthOfTweet
+                // turn text red and disable button
+                if (remainingChars < 0) {
+                    charCount.setTextColor(Color.RED)
+                    btnTweet.isClickable = false
+                    btnTweet.isEnabled = false
+                } else {
+                    charCount.setTextColor(Color.BLACK)
+                    btnTweet.isClickable = true
+                    btnTweet.isEnabled = true
+                }
+                // update remaining chars
+                charCount.setText(remainingChars.toString())
+
+            }
+        })
 
         // Handling the tweet click
         btnTweet.setOnClickListener{
@@ -39,8 +72,8 @@ class ComposeActivity : AppCompatActivity() {
                 Toast.makeText(this,"Empty tweets not allowed!", Toast.LENGTH_SHORT).show()
             }
             //2. Make sure the tweet is under character count
-            else if(tweetContent.length > 140){
-                Toast.makeText(this,"Tweet is too long! Limit is 140 characters", Toast.LENGTH_SHORT).show()
+            else if(tweetContent.length > 280){
+                Toast.makeText(this,"Tweet is too long! Limit is 280 characters", Toast.LENGTH_SHORT).show()
             }
             else{
                 client.publishTweet(tweetContent, object : JsonHttpResponseHandler() {
